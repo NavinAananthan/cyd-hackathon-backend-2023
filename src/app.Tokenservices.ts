444eacae9385ea8d3f03feb2ec10a0d6713739e9
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-01-25 14:01:26 +0530

    commit1

--- src/app.Tokenservices.ts
+++ src/app.Tokenservices.ts
@@ -10,6 +10,7 @@ export class TokenService {
     private covurl='https://api.covalenthq.com/v1/1/address'
     private baseurl='https://api.coingecko.com/api/v3/coins';
 
+
     async getBalances(address:string) {
         const optionethereum={
             uri: `${this.covurl}/${address}/balances_v2/?key=ckey_c0cb6e86390f40ef872c5a783e8`,
@@ -36,7 +37,18 @@ export class TokenService {
         };
         return rp(options);
     }
+    
 
+    async validate(input: string) {
+        const alphanumericRegex = /^[a-zA-Z0-9]*$/;
+        if (input.length < 5 || input.length > 20) {
+          return false;
+        }
+        if (!alphanumericRegex.test(input)) {
+          return false;
+        }
+        return true;
+    }
 
     // To get all the coin details
     async getCoinDetails(id:string) {
@@ -44,30 +56,32 @@ export class TokenService {
           uri: `${this.baseurl}/${id}`,
           json: true,
         };
-        const response=await rp(options)
 
-        return{
-            id:response.id,
-            symbol:response.symbol,
-            name:response.name
-        };
-      }
+        const response=await rp(options)
+        
+        if(await this.validate(response.name)){
+            return{
+                id:response.id,
+                symbol:response.symbol,
+                name:response.name
+            };
+        }
+        else{
+            return{ id:response.id,};
+        }
+        
+    }
 
     //This function is to get details of all the coins
     async getallcoindetails(){
+        let coins:any[]=[];
         const options = {
             uri: `${this.baseurl}`,
             json: true,
           };
         
-
-        return rp(options)
-        /*const response=await rp(options)
-  
-        return{
-            id:response.id,
-            symbol:response.symbol,
-            name:response.name
-        };*/
+        var json=rp(options);
+        coins.push(json);
+        return coins;
     }
 }
