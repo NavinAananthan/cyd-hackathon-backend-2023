7ffac44963f86ee0458cfecf9eb0beb76d16cc8c
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-01-25 13:38:07 +0530

    new

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TokenService } from './app.Tokenservices';
 
 @Controller()
 export class AppController {
@@ -9,4 +10,37 @@ export class AppController {
   async getHello(): Promise<string> {
     return this.appService.getHello();
   }
+
+}
+
+@Controller()
+export class Tokencontroller{
+  constructor(private readonly tokenService:TokenService){}
+
+  @Get('/balances/:address')
+  async getBalances(@Param('address') address: string) {
+    return await this.tokenService.getBalances(address);
+  }
+
+
+  /*@Get('/balances')
+  async getBalances() {
+    return await this.tokenService.getBalances();
+  }*/
+
+  
+  @Get('/list-coins/:id')
+  async getCoin(@Param('id') id: string) {
+    return await this.tokenService.getCoinDetails(id);
+  }
+
+  @Get('/allcoins')
+  async getallCoin() {
+    return await this.tokenService.getallcoindetails();
+  }
+
+
+
+
+
 }
